a7cb3668fd5702ccaea7ded6db2d32042041cc7c
Ayrat Burnashev <airatbyr@ya.ru>
2021-08-25 19:50:30 +0300

    feat: delete frontend project

--- frontend/src/utils/apiUserData.js
+++
@@ -1,5 +0,0 @@
-export const apiUserData = {
-  userAuthorizationToken: "d23f286f-33b5-49bb-ba5f-07eac9751a60",
-  userGroupNumber: "cohort-24",
-  ariBaseUrl: "https://mesto.nomoreparties.co/v1",
-};
--- frontend/src/utils/constants.js
+++
@@ -1,1 +0,0 @@
-export const KEY_TO_CLOSE = "Escape";
