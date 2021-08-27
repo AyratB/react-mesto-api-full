274f8e648e6358ea413ef8ad0522913205e6fac3
Ayrat Burnashev <airatbyr@ya.ru>
2021-08-27 17:29:13 +0300

    feat: add credentials

--- backend/middlewares/corsHandler.js
+++ backend/middlewares/corsHandler.js
@@ -13,6 +13,7 @@ function corsHandler(req, res, next) {
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
   }
 
   if (method === 'OPTIONS') {
